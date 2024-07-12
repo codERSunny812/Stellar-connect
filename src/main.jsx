663e72588ace57856a14b187b83922c9268db662
codersunny812 <sengersunny448@gmail.com>
2024-07-12 16:04:01 +0530

    clerk addded

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,20 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
+import { ClerkProvider } from '@clerk/clerk-react'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Router}/>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={Router} />
+    </ClerkProvider>
+    
   </React.StrictMode>,
 )
