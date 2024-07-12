4a9c3155e7a7cd30572cf199c865f601b09f19c9
codersunny812 <sengersunny448@gmail.com>
2024-07-12 15:59:46 +0530

    icon component

---
+++ src/components/Icons.jsx
@@ -0,0 +1,13 @@
+import './icon.css';
+
+const Icons = ({Icons , name}) => {
+
+  return (
+    <div className="Icons">
+       <Icons className="iconss"/>
+       <p>{name}</p>
+    </div>
+  )
+}
+
+export default Icons
