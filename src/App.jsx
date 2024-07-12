13f56c292560854c426621c4a5a4a5ec45dc6352
codersunny812 <sengersunny448@gmail.com>
2024-07-12 16:02:32 +0530

    clerk added

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@
 import { Outlet, createBrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from './components/Home'
-import Login from './components/Login'
-import Register from './components/Register'
 import Header from './components/Header'
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
+import Base from './components/Base'
+import SignUpPage from './components/SignUpPage'
+import SignInPage from './components/SignInPage';
 
 function App() {
 
 
   return (
   <>
-  <Header/>
-  <Outlet/>
+  <header>
+        <SignedOut>
+          <Base/>
+        </SignedOut>
+        <SignedIn>
+          <Header />
+          <Outlet />
+        </SignedIn>
+  </header>
+
   </>
   )
 }
@@ -30,14 +40,12 @@ export const Router = createBrowserRouter([
       {
        path:'/',
        element:<Home/>
-      },
-      {
-        path: '/login',
-        element: <Login />
-      },
-      {
-        path:'/register',
-        element:<Register/>
+      },{
+      path:'/sign-up',
+      element:<SignUpPage/>
+      },{
+        path: '/sign-in',
+        element: <SignInPage />
       }
     ]
   },
