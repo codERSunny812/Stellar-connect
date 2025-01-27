c3c85ef93e43734b5081178e0465670a3931dd36
codersunny812 <sengersunny448@gmail.com>
2025-01-27 15:28:42 +0530

    home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,29 +2,13 @@ import SideBar from "./SideBar";
 import Feed from "./Feed";
 import Widget from "./Widget";
 import "./home.css";
-import { useEffect} from "react";
-import { addUser } from "../Collections/user.collection.js";
 import { Breathing } from "react-shimmer";
 import useStore from "../store/Store.js";
 
 const Home = () => {
+  // console.log("inside the home component:")
   const userData = useStore((state) => state.userData);
-
   // console.log("user data in the home component:", userData);
-
-  // adding the data of the user to the firebase DB
-  useEffect(() => {
-    const addUserToDb = async () => {
-      if (userData && userData?.id) {
-        try {
-        await addUser(userData);
-        } catch (error) {
-          console.log(`Error adding user to DB: ${error.message}`);
-        }
-      }
-    };
-    addUserToDb();
-  }, [userData]);
 
   // Conditional rendering to wait for userData to be populated
   if (!userData || userData == null) {
