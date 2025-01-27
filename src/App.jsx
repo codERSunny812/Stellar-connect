ef021c05668b247d0fd278905b58671a8c2cbd0a
codersunny812 <sengersunny448@gmail.com>
2025-01-27 15:31:58 +0530

    app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import React, { Suspense, useEffect } from "react";
 import AuthLayout from "./Auth";
-import { UserProfile } from "@clerk/clerk-react";
 import Error from "./Error";
 import SignUpPage from "./components/Signup.jsx";
 import SignInPage from "./components/Signin.jsx";
@@ -12,6 +11,8 @@ import { useUser } from "@clerk/clerk-react";
 import useStore  from './store/Store.js'
 import { Toaster } from 'react-hot-toast';
 import Profile from "./components/Profile.jsx";
+import { allUserData as fetchAllUser , addUser } from "./Collections/user.collection.js";
+
 
 // lazy loading the component
 const MyNetwork = React.lazy(() => import("./components/MyNetwork"));
@@ -20,22 +21,60 @@ const Message = React.lazy(() => import("./components/Message"));
 const Notification = React.lazy(() => import("./components/Notification"));
 
 function App() {
+  // console.log("app component rendered")
   const {user} = useUser(); //clerk user object
-  const { userData,setUser }= useStore((state)=>state);
+  const { userData , setUser, allSignedUpUser, setSignedUpUser }= useStore((state)=>state);
+  // console.log("value of user clerk object in app component:",user);
+  // console.log("value of the  user data from the store in app component:",userData);
+  // console.log("value of the  set user function inside the app component is:",setUser);
+  // console.log("value of the all allSignedUpUser variable inside the app component is:",allSignedUpUser);
+  // console.log("value of the setSignedUpUser func inside the app component is:",setSignedUpUser);
+  
 
-  //update the state of the store with the clerk user data
-  useEffect(() => {
-    if (user && (!userData || user.id !== userData.id)) {
-      const loggedInUserData = {
-        id: user.id,
-        fullName: user.fullName,
-        imageUrl: user.imageUrl,
-        firstName: user.firstName,
-      };
+  //uploding the data of the user into the user's collection
+   useEffect(()=>{
+    try {
+      // console.log("uploading the user data into the firestore collection:")
+      if(user){
+        const userInfo = {
+          id: user.id,
+          fullName: user.fullName,
+          imageUrl: user.imageUrl,
+          firstName: user.firstName,
+        }
 
-      setUser(loggedInUserData);
+        const addUserToDb = async () => {
+             if (userInfo && userInfo?.id) {
+               try {
+                const data =  await addUser(userInfo);
+                setUser(data);
+                // console.log("user data is upadted in the zustand store:")
+               } catch (error) {
+                 console.log(`Error adding user to DB: ${error.message}`);
+               }
+             }
+           };
+           addUserToDb();
+      }
+     } catch (error) {
+       console.error("error in adding the user into the DB",error.message)
     }
-  }, [user, userData, setUser]);
+  },[user])
+
+
+
+// update the all user data 
+useEffect(()=>{
+  // console.log("fetching the data of all the logged in user from the db")
+const fetchedAllUser = async()=>{
+const allUserData = await fetchAllUser();
+setSignedUpUser(allUserData);
+// console.log("updating the state of the user in the db.")
+}
+
+fetchedAllUser();
+},[user,userData])
+
 
   
   return (
