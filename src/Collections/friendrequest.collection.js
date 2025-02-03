1b63dfecbed225f4b131cfe2064692ee1e95f995
codersunny812 <sengersunny448@gmail.com>
2025-02-03 21:36:59 +0530

    friend request collection

---
+++ src/Collections/friendrequest.collection.js
@@ -0,0 +1,193 @@
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  doc,
+  addDoc,
+  serverTimestamp,
+  deleteDoc,
+  setDoc,
+  onSnapshot,
+  getDoc,
+} from "firebase/firestore";
+import { db } from "../constants/Firebase.config";
+
+// function to sent friend request
+export const sendFriendRequest = async (currentId, targetId) => {
+  console.log("inside the send friend request function:")
+  console.log("both id are:", currentId, targetId);
+  //prior checking
+  if (!currentId || !targetId) {
+    console.error("not a valid id");
+    return;
+  }
+
+  try {
+    //make a refrence to the collection
+    const friendRequestRef = collection(db, "friendRequest");
+
+    // checking that friend request is already sent or not
+    const q = query(
+      friendRequestRef,
+      where("userId", "==", currentId),
+      where("friendId", "==", targetId)
+    );
+
+    const querySnapShot = await getDocs(q);
+
+    if (!querySnapShot.empty) {
+      console.log("friend request has been already sent");
+      return;
+    }
+
+    // add the new friend
+    await addDoc(friendRequestRef, {
+      userId: currentId,
+      friendId: targetId,
+      status: "pending", //status is pending when the request is sent
+      timeStamp: serverTimestamp(),
+    });
+  } catch (error) {
+    console.error("unable to add friends", error.message);
+  }
+};
+
+// function to handle friend request
+export const handleFriendRequest = async (
+  requestId,
+  currentUserId,
+  targeteduserId,
+  action
+) => {
+  if (!currentUserId || !targeteduserId) {
+    console.log("not a valid id found");
+    return;
+  }
+
+  console.log(requestId);
+
+  try {
+    console.log("inside the handle friend request function");
+    // making a refrence to the friend request collection
+    const requestRef = doc(db, "friendRequest", requestId);
+    console.log("request model ref:", requestRef);
+    const friendCollectionRef = collection(db, "friends");
+    console.log("friends collection ref:", friendCollectionRef);
+
+    // add the friend if the user accept the request
+    if (action === "accept") {
+      console.log("inside the accept block");
+      await setDoc(
+        doc(friendCollectionRef, `${currentUserId}_${targeteduserId}`),
+        {
+          userId: currentUserId,
+          friendId: targeteduserId,
+          timeStamp: serverTimestamp(),
+        }
+      );
+    }
+    //delete the data either the user accept the request or reject the request
+    await deleteDoc(requestRef);
+  } catch (error) {
+    console.error("error in handling the friend request", error.message);
+  }
+};
+
+export const pendingRequest =async (userId, cb) => {
+  console.log("inside the peding request section")
+  if (!userId) {
+    console.log("User ID not found");
+    return;
+  }
+
+  try {
+    console.log("inside the pending request function:")
+    const friendRequestRef = collection(db, "friendRequest");
+
+    // Fetch requests where the user is either the sender or the receiver
+    const qSent = query(friendRequestRef, where("userId", "==", userId));  // Sent requests
+    const qReceived = query(friendRequestRef, where("friendId", "==", userId));  // Received requests
+
+    
+    // for the sender 
+    const unsubscribeSent = onSnapshot(qSent, async (querySnapshot) => {
+
+      const sentRequests = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+        type: "sent",
+      }));
+
+      console.log("sent request:",sentRequests)
+
+      // now get the detail of the user 
+      const friendsData = await Promise.all(
+              sentRequests.map(async (data) => {
+
+                const userRef = doc(db, "users", data.friendId);
+                const userData = await getDoc(userRef)
+
+                return {
+                  ...data,
+                  friendName: userData.data()?.fullName, // Add user name
+                  friendProfilePic: userData.data()?.avatar, // Add profile picture (if available)
+                };
+              })
+            );
+
+            console.log("friend data to whom we have sent requst:",friendsData)
+
+            cb(friendsData);
+    });
+
+    const unsubscribeReceived = onSnapshot(qReceived, async (querySnapshot) => {
+
+      const receivedRequests = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+        type: "received",
+      }));
+
+      // now get the detail of the user 
+      const friendsData = await Promise.all(
+        receivedRequests.map(async (data) => {
+          const userRef = doc(db, "users", data.userId);
+          const userData = await getDoc(userRef)
+
+          return {
+            ...data,
+            friendName: userData.data()?.fullName, // Add user name
+            friendProfilePic: userData.data()?.avatar, // Add profile picture (if available)
+          };
+        })
+      );
+
+      cb(friendsData);
+    });
+
+    return () => {
+      unsubscribeSent();
+      unsubscribeReceived();
+    };
+  } catch (error) {
+    console.error("Error fetching pending requests", error.message);
+    return [];
+  }
+};
+
+
+// function to delete the request 
+export const deleteRequest = async(requestId)=>{
+try {
+  // talking the refrence of the friedn request collection
+  const requestDataRef = doc(db,"friendRequest",requestId);
+
+  await deleteDoc(requestDataRef);
+  console.log("requrst deleted successfully")
+
+} catch (error) {
+  console.error("error in deleting the request:");
+  return;
+}
+}
